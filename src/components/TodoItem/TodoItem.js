5b87f14c97c6a424d0c5868a467b21ba28a7f05a
김서짱 <seojeong1101@gmail.com>
2021-08-16 00:28:21 +0900

    first commit

---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import styles from './TodoItem.scss';
+import classNames from 'classnames/bind';
+
+
+const cx = classNames.bind(styles);
+
+class TodoItem extends Component{
+
+    shouldComponentUpdate(nextProps,nextState){
+        return this.props.done !== nextProps.done;
+    }
+
+    render(){
+        //비구조화 할당
+        const {done,children,onToggle,onRemove} = this.props;
+
+        return(
+            <div className={cx('todo-item')} onClick={onToggle}>
+                <input className={cx('tick')} type="checkbox" checked={done} readOnly/>
+                <div className={cx('text',{done})}>{children}</div>
+                <div className={cx('delete')} onClick={(e)=>{
+                    onRemove();
+                    e.stopPropagation();}}>[지우기]</div>
+            </div>
+        );
+    }
+}
+
+export default TodoItem;
